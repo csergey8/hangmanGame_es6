03150d2012c2e977b081534189969ce14546c19e
csergey8 <c.sergey@protonmail.com>
2018-05-27 10:37:49 +0300

    added data into game

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
+const reset = document.querySelector('#reset');
+let game1;
 
-const game1 = new Hangman('word main', 5);
+const render = () => {
+    puzzleEl.textContent = game1.puzzle;
+    guessesEl.textContent = game1.statusMsg;
+}
 
-puzzleEl.textContent = game1.puzzle;
-guessesEl.textContent = game1.statusMsg;
+const startGame = async () => {
+    const puzzle = await getPuzzle(2);
+    console.log(puzzle);
+    game1 = new Hangman(puzzle, 5);
+    render();
+}
 
+reset.addEventListener('click', startGame);
 
+startGame();
 
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode);
     game1.makeGuesse(guess);
-    puzzleEl.textContent = game1.puzzle;
-    guessesEl.textContent = game1.statusMsg;
+    render();
 });
 
-getPuzzle(5)
-    .then((puzzle) => {
-        console.log(puzzle);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+// getPuzzle(5)
+//     .then((puzzle) => {
+//         console.log(puzzle);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
 
 
-const createTipper = (tip) => {
-    return (amount) => {
-        return amount * (tip / 100);
-    }
-}
 
-getLocation().then((data) => {
-    return getCountry(data.country) 
-}).then((country) => {
-    console.log(country)
-})
+// getLocation().then((data) => {
+//     return getCountry(data.country) 
+// }).then((country) => {
+//     console.log(country)
+// })
 
-getCurrentCountry().then((country) => {
-    console.log(country);
-})
+// getCurrentCountry().then((country) => {
+//     console.log(country);
+// })
 
 
